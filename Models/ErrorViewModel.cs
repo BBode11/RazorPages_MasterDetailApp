6860497d3da8309c79d26479046d5b74b324e9ab
BBode11 <bode11@mail.nmc.edu>
2021-09-21 21:04:50 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RazorPages_MasterDetailApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
